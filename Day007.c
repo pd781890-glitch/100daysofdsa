60cb584a79207ea6456799e013a1c06c438d2e2d
pd781890 <pd781890@gmail.com>
2026-02-07 10:00:00 +0530

    Day 7 - DSA practice

---
+++ Day007.c
@@ -0,0 +1,13 @@
+// Day 007 of #100daysofdsa
+#include<stdio.h>
+int fib(int n){
+if(n==0)return 0;
+if(n==1)return 1;
+return fib(n-1)+fib(n-2);
+}
+int main(){
+int n;
+scanf("%d",&n);
+printf("%d",fib(n));
+return 0;
+}
