ee92fdfcf7704e54eab3106ed590302bb3ce032e
pd781890 <pd781890@gmail.com>
2026-02-09 10:00:00 +0530

    Day 9 - DSA practice

---
+++ Day009.c
@@ -0,0 +1,17 @@
+// Day 009 of #100daysofdsa
+#include<stdio.h>
+#include<string.h>
+int main(){
+char s[1000];
+scanf("%s",s);
+int l=0,r=strlen(s)-1;
+while(l<r){
+char t=s[l];
+s[l]=s[r];
+s[r]=t;
+l++;
+r--;
+}
+printf("%s",s);
+return 0;
+}
