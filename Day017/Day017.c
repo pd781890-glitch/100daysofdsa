1b578b8e28ef8ec5436636a05ab98d1eb7c0e2af
pd781890 <pd781890@gmail.com>
2026-02-17 12:00:00 +0530

    Day 017: Backdated to Feb 17

---
+++ Day017/Day017.c
@@ -0,0 +1,16 @@
+// Day 017 of #100daysofdsa
+#include<stdio.h>
+int main(){
+int n;
+scanf("%d",&n);
+int arr[n];
+for(int i=0;i<n;i++)scanf("%d",&arr[i]);
+int max=arr[0],min=arr[0];
+for(int i=1;i<n;i++){
+if(arr[i]>max)max=arr[i];
+if(arr[i]<min)min=arr[i];
+}
+printf("Max: %d\n",max);
+printf("Min: %d",min);
+return 0;
+}
